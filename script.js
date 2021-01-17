830fd19343b40ee9374cc392fdf0e1137bf0296c
Patrick Brandt <brandt.patrickc@gmail.com>
2021-01-17 13:24:47 -0800

    Added a section for each question and section id for correct answer. Added bootstrap bundle scripts.Started to add styling on css.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// To-do list
+// Set up questions in html. Assign a correct answer ID? And maybe a class to wrong answers.
+// Create a start button. Un click, start timer.
+    //set interval?
+// When you answer a question correctly, You are presented with a new question. Look up color excercise in class video.
+    // Assign id's to divs.
+// When you answer a question incorrectly, time is subtracted from the clock.
+// When question is answered correctly, add points to score. 
+// When all questions answered or time reaches 0, the GAME OVER.
+// When game over, user can enter their initials and score.
+//
+//
+//
 
