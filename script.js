a230cd481350d31c9c8c96a4845e353612123a19
Patrick Brandt <brandt.patrickc@gmail.com>
2021-01-18 15:10:55 -0800

    Adding more notes to js file as i no. No new working code.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // To-do list
-// Set up questions in html. Assign a correct answer ID? And maybe a class to wrong answers.
-// Create a start button. Un click, start timer.
+
+// Create a start button. on click, start timer.
     //set interval?
-// When you answer a question correctly, You are presented with a new question. Look up color excercise in class video.
-    // Assign id's to divs.
+    //hide "start" section when start button is clicked, and display qestion 1.
+// When you answer a question correctly, add points to score. You are then presented with a new question. Look up color excercise in class video maybe.
+    // when correct, hide question 1, and display question 2.
 // When you answer a question incorrectly, time is subtracted from the clock.
-// When question is answered correctly, add points to score. 
-// When all questions answered or time reaches 0, the GAME OVER.
+//  
+// When all questions answered or time reaches 0, then GAME OVER.
 // When game over, user can enter their initials and score.
 
 //I was trying to hide section 1, and it worked. I think I need to use this in order to move from question to question. For example, "if user clicks start, hide section 1,3,4,5. Not sure how it will work yet.
