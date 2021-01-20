339bbdfee93b81de895ba20853e920a1cf3fcfe2
Patrick Brandt <brandt.patrickc@gmail.com>
2021-01-19 18:31:32 -0800

    TA added code and comments.

--- script.js
+++ script.js
@@ -20,42 +20,52 @@
 //  question4.setAttribute("style",)"display:none;");
 //  question5.setAttribute("style",)"display:none;");
 //};    
-
+var currentQuestion = 1;
 var correct = document.querySelector(".correct");
 var wrong = document.querySelector(".wrong");
 var countdown = document.querySelector(".countdown");
 var startButton = document.querySelector("startButton");
 var winCounter = 0;
 var loseCounter = 0;
-
+var points = 0;
 var timer;
 var timerCount;
+function showQuestion () {
+    // target all sections, and add class of hidden
+    // target section with id of `currentQuestion`...
+    // const currentSection = document.getElementById(currentQuestion.toString())
+    // removed hidden class
+};  
 
 //This function is called when the user clicks the start button.
-function startGame () {
+// function startGame () {
   
-    timerCount = 75;
-    startTimer ()
-}
+//     timerCount = 75;
+//     startTimer ()
+// }
 
-function startTimer() {
-    // Sets the timer
-    timer = setInterval(function() {
-        timerCount--;
-        countdown.textContent = timerCount;
-        if (timerCount <=0) {
-            //clears interval and stops timer.
-            clearInterval(timer);
+// function startTimer() {
+//     // Sets the timer
+//     timer = setInterval(function() {
+//         timerCount--;
+//         countdown.textContent = timerCount;
+//         if (timerCount <=0) {
+//             //clears interval and stops timer.
+//             clearInterval(timer);
 
-        }
+//         }
     
-    },1000);
-}
+//     },1000);
+// }
 
 
-// Event listener starts game when user clicks the startbutton.
-startButton.addEventListener("click",startGame);
+// // Event listener starts game when user clicks the startbutton.
+// startButton.addEventListener("click",startGame);
 
 //Event Listener for correct answers or an I good with the quesryselector above? Then do I have the event listener trigger the hide question function?
-correct.addEventListener("click",//Do I add in the hide question function here?)
+//correct.addEventListener("click",//Do I add in the hide question function here?)
 //Event listener for incorrect answers.
+var section = document.getElementById(currentQuestion.toString())
+section.addEventListener('click', function(e) {
+    console.log('e: ', e.target.dataset.answer)
+})
